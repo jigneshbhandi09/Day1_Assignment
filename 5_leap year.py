b8c97c8a35846b698494e346bfd21fa2652eea28
jigneshbhandi09 <95614233+jigneshbhandi09@users.noreply.github.com>
2024-06-26 11:35:01 +0530

    5_leap_year.py

--- 5_leap year.py
+++ 5_leap year.py
@@ -1,7 +1,10 @@
-num = int(input('Enter a number:'))
+num = int(input('Enter a number: '))
 
-if (num%400==0 and num%100==0):
+if (num % 400 == 0):
     print(num, 'is a leap year')
-    
+elif (num % 100 == 0):
+    print(num, 'is not a leap year')
+elif (num % 4 == 0):
+    print(num, 'is a leap year')
 else:
     print(num, 'is not a leap year')
