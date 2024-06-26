a2a02fa51dbdfa64f11f36b1c777249ae903cb7e
jigneshbhandi09 <95614233+jigneshbhandi09@users.noreply.github.com>
2024-06-26 11:33:57 +0530

    Update 3_comment.py

--- 3_comment.py
+++ 3_comment.py
@@ -1,1 +1,14 @@
+num = int(input('Enter a number: ')) #take a number from user
+
+if (num % 400 == 0):
+    # If the number is divisible by 400, it is a leap year
+    print(num, 'is a leap year')
+elif (num % 100 == 0):
+    # If the number is divisible by 100 but not by 400, it is not a leap year
+    print(num, 'is not a leap year')
+elif (num % 4 == 0):
+    # If the number is divisible by 4 but not by 100, it is a leap year
+    print(num, 'is a leap year')
+else:
+    print(num, 'is not a leap year')
 
